d536a354439b45f5f99881300bfd5c5fd016bf09
soumya <soumya6367@gmail.com>
2022-01-15 10:17:59 -0800

    day 5 otp conformation is valid

--- RTP/app1/urls.py
+++ RTP/app1/urls.py
@@ -5,5 +5,13 @@ urlpatterns = [
     path('',views.showindex,name='main'),
     path('registration/',views.registration,name='registration'),
     path('user_registration/',views.registration,name='user_registration'),
+    path('validate_otp/',views.validate_otp,name='validate_otp'),
+
+    #otp-page
+    path('user_otp/',views.userotp,name='user_otp'),
+    #conformation
+    path('conformation/',views.conformation,name='conformation'),
+    #login page
+    path('login/',views.login,name='login'),
 ]
 
--- RTP/app1/views.py
+++ RTP/app1/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from app1.forms import RegistrationForm
+from app1.models import RegistrationModel
+from django.contrib.messages import success
 
 # Create your views here.
 def showindex(request):
@@ -15,8 +17,40 @@ def registration(request):
             #rf.otp = 5475
             #rf.status = 'pending'#Not Write status in this place means this status write in default in models thats way
             rf.save()
+            return redirect('user_otp')
 
         else:
             return render(request,'app1_temp/Registration.html',{'form':rf})
     else:
         return render(request,'app1_temp/Registration.html',{'form':rf})
+
+
+def userotp(request):
+    return render(request,'app1_temp/otp.html')
+
+
+def validate_otp(request):#get--it will return only one object
+    try:
+        result = RegistrationModel.objects.get(contact=request.POST.get('t1'),otp=request.POST.get('t2'))
+        #print(result.status)
+        if result.status == 'pending':
+            result.status == 'approved'#updating the record
+            result.save()
+            success(request,'Thank You For Registration ')
+            return redirect('conformation')
+        elif result.status == 'approved':
+            success(request,'Your Registration Is Already Approved')
+            return redirect('conformation')
+
+    except RegistrationModel.DoesNotExist:
+        message='Sorry This User Is Invalid'
+        return render(request,'app1_temp/otp.html',{'message':message})
+
+
+
+def conformation(request):
+    return render(request,'app1_temp/conformation.html')
+
+
+def login(request):
+    return render(request,'app1_temp/login.html')
